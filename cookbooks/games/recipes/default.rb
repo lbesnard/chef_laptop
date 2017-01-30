cd4b35dfb09e013fc3dc893ae3d3a19a740b72bc
lbesnard <laurent.besnard@utas.edu.au>
2017-01-30 12:08:55 +1100

    remove defunct games ppa

--- cookbooks/games/recipes/default.rb
+++ cookbooks/games/recipes/default.rb
@@ -1,25 +1,6 @@
 HOME_DIR = "/home/#{node['chef_laptop']['user']}"
 
 
-##  quake repo
-execute "zandronum" do
- command "add-apt-repository 'deb http://debian.drdteam.org/ stable multiverse';wget -O - http://debian.drdteam.org/drdteam.gpg | apt-key add -;apt-get update;"
-  action :run
-  environment ({'HOME' => '#{HOME_DIR}'})
-  ignore_failure true
-  user "root"
-end
-
-
-## snes repo
-execute "snes9x-gtk" do
- command  "add-apt-repository -y ppa:bearoso/ppa; apt-get update;"
-  action :run
-  environment ({'HOME' => '#{HOME_DIR}'})
-  ignore_failure true
-  user "root"
-end
-
 # cleaning
 execute "cleaning" do
  command  "apt-get install --assume-yes force;apt-get update;"
@@ -29,16 +10,8 @@ execute "cleaning" do
   user "root"
 end
 
-# retroach
-#execute "retroarch" do
- #command  "add-apt-repository -y ppa:hunter-kaller/ppa; apt-get update;"
-  #action :run
-  #environment ({'HOME' => '#{HOME_DIR}'})
-  #ignore_failure true
-  #user "root"
-#end
 
-games = %w{dos2unix doom-wad-shareware doomseeker doomseeker-zandronum gzdoom ioquake3 ioquake3-dbg ioquake3-server zsnes playonlinux wine1.6  zandronum zandronum-client zandronum-pk3 zandronum-server}
+games = %w{mess dos2unix doom-wad-shareware doomseeker doomseeker-zandronum gzdoom ioquake3 ioquake3-dbg ioquake3-server zsnes playonlinux wine1.6}
 packages = [games]
 
 packages.flatten.each do |a_package|
